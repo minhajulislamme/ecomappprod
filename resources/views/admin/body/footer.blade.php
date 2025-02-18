c0b354b4d9c765b322c50d996fc200e263f79e46
minhajulislamme <minhajulislam150125@gmail.com>
2025-02-18 23:10:48 +0600

    fix footer and image preview

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,5 +1,6 @@
 <div
     class="fixed bottom-0 left-0 w-full py-2 px-6 bg-white flex flex-col items-center justify-center shadow-md shadow-black/5 z-30">
-    <div class="text-xs text-gray-500">&copy; 2025 Tech Ever IT. All rights reserved || Design and Develop by ❤️ Minhajul
+    <div class="text-xs text-center text-gray-500">&copy; 2025 Tech Ever IT. All rights reserved || Design and Develop by
+        ❤️ Minhajul
         Islam</div>
 </div>
